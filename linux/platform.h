ef3be1d49fff33bd7fc429c7df2dcb7c83ac66ea
Maple_Miles <maplemilesalt@gmail.com>
2026-09-11 15:47:28 -0300

    Expand Linux platform compatibility types

--- linux/platform.h
+++ linux/platform.h
@@ -15,5 +15,33 @@ typedef int BOOL;
 #endif
 
 typedef uint32_t COLORREF;
+typedef int32_t LONG;
+typedef uint32_t DWORD;
+typedef uint32_t UINT;
+typedef int INT;
+typedef uint16_t WORD;
+typedef uintptr_t ULONG_PTR;
+typedef uintptr_t WPARAM;
+typedef intptr_t LPARAM;
+typedef intptr_t LRESULT;
+typedef char TCHAR;
+typedef char *LPTSTR;
+typedef const char *LPCTSTR;
+typedef void *HWND;
+typedef void *HINSTANCE;
+typedef void VOID;
+
+#ifndef APIENTRY
+#define APIENTRY
+#endif
+#ifndef CALLBACK
+#define CALLBACK
+#endif
+#ifndef NEAR
+#define NEAR
+#endif
+#ifndef TEXT
+#define TEXT(x) x
+#endif
 
 #endif /* CALC_LINUX_PLATFORM_H */
